3196366a597105add00515b0d8d55f7abee61065
Vishal Singh <vishalsingh@cedcomemrce.com>
2025-05-03 19:24:46 +0530

    feat: lib commonjs

--- lib/typescript/src/components/index.d.ts
+++
@@ -1,8 +0,0 @@
-export { Button } from './Button/Button';
-export { Table } from './Table/Table';
-export { Select } from './Select/Select';
-export { TextField } from './TextField/TextField';
-export { RadioButton } from './RadioButton/RadioButton';
-export { Switcher } from './Switcher/Switcher';
-export { FabButton } from './FabButton/FabButton';
-//# sourceMappingURL=index.d.ts.map
--- lib/typescript/src/theme/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { useTheme, ThemeProvider } from './ThemeContext';
-//# sourceMappingURL=index.d.ts.map
