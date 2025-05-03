66dfdf588961680c7646ee658483fa34bbb1362b
Vishal Singh <vishalsingh@cedcomemrce.com>
2025-05-03 19:25:38 +0530

    feat: lib commonjs

---
+++ lib/typescript/src/components/FabButton/FabButton.d.ts
@@ -0,0 +1,14 @@
+/**
+ * @license
+ * Copyright 2025 Aaskplanets
+ */
+import React from 'react';
+interface FabButtonProps {
+    label?: string;
+    icon?: string;
+    onPress?: () => void;
+    disabled?: boolean;
+}
+export declare const FabButton: React.FC<FabButtonProps>;
+export {};
+//# sourceMappingURL=FabButton.d.ts.map
---
+++ lib/typescript/src/theme/theme.d.ts
@@ -0,0 +1,18 @@
+export declare const lightTheme: {
+    background: string;
+    text: string;
+    primary: string;
+    secondary: string;
+    buttonText: string;
+    border: string;
+};
+export declare const darkTheme: {
+    background: string;
+    text: string;
+    primary: string;
+    secondary: string;
+    buttonText: string;
+    border: string;
+};
+export type ThemeType = typeof lightTheme;
+//# sourceMappingURL=theme.d.ts.map
